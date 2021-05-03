26c0e36555c5af3e871e8878eb497bc8ee14f5bd
joseph-marlitt <joseph.marlitt@gmail.com>
2021-05-03 17:21:08 -0400

    photo work

--- dist/js/main.js
+++ dist/js/main.js
@@ -34,3 +34,12 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+$(".photo-gallery img").click(function(){
+  $("#full-image").attr("src", $(this).attr("src"));
+  $('#image-viewer').show();
+});
+
+$("#image-viewer .close").click(function(){
+  $('#image-viewer').hide();
+});
